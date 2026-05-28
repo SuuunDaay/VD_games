ff0779e993df08f38206d48a0e0e90422fc45951
Даниил Лучин <boomb1t77u@gmail.com>
2026-05-28 10:57:50 +0300

    Initial project setup: uv init, package structure, VD-games command

---
+++ VD_games/scripts/VD_games.py
@@ -0,0 +1,6 @@
+def main() -> None:
+    print("Welcome to the VD_Games!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ VD_games/scripts/VD_main.py
@@ -0,0 +1,5 @@
+from VD_games.scripts.VD_games import main
+
+
+if __name__ == "__main__":
+    main()
